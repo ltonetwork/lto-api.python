dc3b1b537cd2bc2a3a3bb94f2605aece893a00aa
Andrea Scorza <atlantas95@gmail.com>
2021-08-17 18:25:35 +0200

    working on argparser

---
+++ bash.py
@@ -0,0 +1,40 @@
+import argparse
+import sys
+
+
+def main():
+
+    '''
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+    #print(parser.parse_args(['7', '-1', '42']))'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x', type=float, default=1.0,
+                        help='What is the first number?')
+    parser.add_argument('--y', type=float, default=1.0,
+                        help='What is the second number?')
+    parser.add_argument('--operation', type=str, default='add',
+                        help='What operation? Can choose add, sub, mul, or div')
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)))
+
+
+def calc(args):
+    if args.operation == 'add':
+        return args.x + args.y
+    elif args.operation == 'sub':
+        return args.x - args.y
+    elif args.operation == 'mul':
+        return args.x * args.y
+    elif args.operation == 'div':
+        return args.x / args.y
+
+
+if __name__ == '__main__':
+    main()
