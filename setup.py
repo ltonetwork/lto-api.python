4b6c2beccfc8579d0d11a99dc7040d5c5568c5bc
Andrea Scorza <47422335+AndreaScorza@users.noreply.github.com>
2021-09-13 18:59:51 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lto-api',
+    version='',
+    packages=find_packages(include=['PyCLTO', 'PyCLTO.*']),
+    package_dir={'': 'PyCLTO'},
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
