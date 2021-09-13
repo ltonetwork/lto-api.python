4c3d19d80b0f5fdaaf4a8b371e70b01a395f23dd
Andrea Scorza <atlantas95@gmail.com>
2021-09-13 21:58:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='lto-api',
     version='',
-    packages=['PyCLTO'],
-    package_dir={'': 'PyCLTO'},
+    packages=find_packages(),
     url='',
     license='',
     author='',
