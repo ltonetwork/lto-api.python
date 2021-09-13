092f8f9b6ff01b8683bccf22ebbb8e21fdeb2494
Andrea Scorza <47422335+AndreaScorza@users.noreply.github.com>
2021-09-13 18:12:31 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lto-api.python',
+    version='',
+    packages=[''],
+    package_dir={'': 'PyCLTO'},
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
