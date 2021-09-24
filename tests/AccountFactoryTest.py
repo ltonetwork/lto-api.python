be853fc8d703912f0e3701b99f23d81ade593933
Andrea Scorza <atlantas95@gmail.com>
2021-09-24 09:15:42 +0200

    Added Test to Create From Private Key Account Factory

--- tests/AccountFactoryTest.py
+++ tests/AccountFactoryTest.py
@@ -62,5 +62,17 @@ class AccountTest(unittest.TestCase):
         seedPhrase = self.factory.generateSeedPhrase()
         self.assertIs(15, len(seedPhrase.split()))
 
+    def testCreateFromPrivateKey(self):
+        privateKey = '3SSiggbbRiZHydbjjwV5PvvexFmWXmV4m3PV54jTkVjCgAFWrGCnxBCkbucTfFNcjxhxnmSxScUswBMmJGyUQW2M'
+        privateKey2 = b'y\xf9\xb6\x85\xb7\xae\x8f\xd0\x15\x8aX\xb2$\xc7\xcbp\xe4\xcbR+2\xf2\x1a8qr\xf5\xb8!\xc4+\x95'
+        publicKey = 'AneNBwCMTG1YQ5ShPErzJZETTsHEWFnPWhdkKiHG6VTX'
+        address = '3N5PoiMisnbNPseVXcCa5WDRLLHkj7dz4Du'
+        expectedAccount = Account(address=address, publicKey=VerifyKey(base58.b58decode(publicKey)), privateKey=privateKey)
+        expectedAccount2 = Account(address=address, publicKey=VerifyKey(base58.b58decode(publicKey)), privateKey=privateKey2)
+        account = self.factory.createFromPrivateKey(privateKey)
+        Tools().__eq__(expectedAccount, account)
+        Tools().__eq__(expectedAccount2, account)
+
+
 if __name__ == '__main__':
     unittest.main()
