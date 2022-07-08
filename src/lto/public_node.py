fd097ec340ad150888caf1a220dfa50ced1b9832
arnold <arnold@jasny.net>
2022-07-08 11:58:54 +0200

    Rename PublicNode `wrapper` method to `request`.

--- src/lto/public_node.py
+++ src/lto/public_node.py
@@ -10,12 +10,12 @@ class PublicNode(object):
     def __init__(self, url, api_key=''):
         self.url = url
         self.api_key = api_key
-        
+
     @staticmethod
     def __addr(account_or_address):
         return account_or_address.address if isinstance(account_or_address, Account) else account_or_address
 
-    def wrapper(self, api, post_data='', host='', headers=None):
+    def request(self, endpoint, post_data='', host='', headers=None):
         if headers is None:
             headers = {}
 
@@ -26,10 +26,10 @@ class PublicNode(object):
             headers = {"X-API-Key": self.api_key}
 
         if post_data:
-            r = requests.post('%s%s' % (host, api), data=post_data,
+            r = requests.post('%s%s' % (host, endpoint), data=post_data,
                               headers=crypto.merge_dicts(headers, {'content-type': 'application/json'}))
         else:
-            r = requests.get('%s%s' % (host, api), headers=headers)
+            r = requests.get('%s%s' % (host, endpoint), headers=headers)
 
         if r.status_code != 200:
             method = 'POST' if post_data else 'GET'
@@ -37,9 +37,9 @@ class PublicNode(object):
                 error = json.loads(r.text)
             except:
                 error = r.text
-            
+
             raise Exception(
-                '{} {}{} responded with {} {}'.format(method, host, api, r.status_code, r.reason),
+                '{} {}{} responded with {} {}'.format(method, host, endpoint, r.status_code, r.reason),
                 error
             )
 
@@ -49,69 +49,64 @@ class PublicNode(object):
 
     def broadcast(self, transaction):
         data = json.dumps(transaction.to_json())
-        response = self.wrapper(api='/transactions/broadcast', post_data=data)
+        response = self.request(endpoint='/transactions/broadcast', post_data=data)
         return tx_from_data(response)
 
     def compile(self, script_source):
-        compiled_script = self.wrapper(api='/utils/script/compile', post_data=script_source)['script']
+        compiled_script = self.request(endpoint='/utils/script/compile', post_data=script_source)['script']
         return SetScript(compiled_script)
 
     def height(self):
-        return self.wrapper('/blocks/height')['height']
+        return self.request('/blocks/height')['height']
 
     def last_block(self):
-        return self.wrapper('/blocks/last')
+        return self.request('/blocks/last')
 
     def block(self, n):
-        return self.wrapper('/blocks/at/%d' % n)
+        return self.request('/blocks/at/%d' % n)
 
     def tx(self, id):
-        response = self.wrapper('/transactions/info/%s' % id)
+        response = self.request('/transactions/info/%s' % id)
         return tx_from_data(response)
 
     def lease_list(self, address):
-        return self.wrapper(api='/leasing/active/{}'.format(self.__addr(address)))
+        return self.request(endpoint='/leasing/active/{}'.format(self.__addr(address)))
 
     def get_data(self, address):
-        return self.wrapper(api='/addresses/data/{}'.format(self.__addr(address)))
+        return self.request(endpoint='/addresses/data/{}'.format(self.__addr(address)))
 
     def get_data_by_key(self, address, key):
-        return self.wrapper(api='/addresses/data/{}/{}'.format(self.__addr(address), key))
+        return self.request(endpoint='/addresses/data/{}/{}'.format(self.__addr(address), key))
 
     def sponsorship_list(self, address):
-        return self.wrapper(api='/sponsorship/status/{}'.format(self.__addr(address)))
+        return self.request(endpoint='/sponsorship/status/{}'.format(self.__addr(address)))
 
     def association_list(self, address):
-        return self.wrapper(api='/associations/status/{}'.format(self.__addr(address)))
+        return self.request(endpoint='/associations/status/{}'.format(self.__addr(address)))
 
     def node_status(self):
-        return self.wrapper(api='/node/status')
+        return self.request(endpoint='/node/status')
 
     def balance(self, address):
-        try:
-            return self.wrapper('/addresses/balance/%s' % self.__addr(address))['balance']
-        except:
-            return -1
+        return self.request('/addresses/balance/%s' % self.__addr(address))['balance']
 
     def balance_details(self, address):
-        return self.wrapper('/addresses/balance/details/%s' % self.__addr(address))
+        return self.request('/addresses/balance/details/%s' % self.__addr(address))
 
     def validate_address(self, address):
-        return self.wrapper('/addresses/validate/{}'.format(address))['valid']
-
+        return self.request('/addresses/validate/{}'.format(address))['valid']
 
     def data_of(self, address):
-        data = self.wrapper('/addresses/data/%s' % self.__addr(address))
+        data = self.request('/addresses/data/%s' % self.__addr(address))
         dict = {}
         for entry in data:
             dict[entry['key']] = entry['value']
         return dict
 
     def transactions(self, address, limit=100, after=''):
-        return self.wrapper('/transactions/address/%s/limit/%d%s' % (
+        return self.request('/transactions/address/%s/limit/%d%s' % (
             self.__addr(address), limit, "" if after == "" else "?after={}".format(after)))
 
     def sign_transaction(self, transaction):
         data = json.dumps(transaction.to_json())
-        return(self.wrapper(api='/transactions/sign', post_data=data))
-
+        return self.request(endpoint='/transactions/sign', post_data=data)
--- tests/public_node_test.py
+++ tests/public_node_test.py
@@ -5,20 +5,20 @@ from lto.transactions.transfer import Transfer
 import pytest
 import requests
 
-class resp():
+
+class Resp:
     def __init__(self, code, text):
         self.status_code = code
         self.text = text
 
     def json(self):
-        return ({"code": self.status_code,
-                 "text": self.text})
+        return {"code": self.status_code, "text": self.text}
 
     def raise_for_status(self):
         pass
 
-class TestPublicNode:
 
+class TestPublicNode:
     ACCOUNT_SEED = "df3dd6d884714288a39af0bd973a1771c9f00f168cf040d6abb6a50dd5e055d8"
     account = AccountFactory('T').create_from_seed(ACCOUNT_SEED)
     node = PublicNode('https://tesnet.lto.network')
@@ -27,23 +27,22 @@ class TestPublicNode:
         node = PublicNode('https://nodes.lto.network')
         assert node.url == 'https://nodes.lto.network'
 
-    @mock.patch.object(requests, 'get', return_value=resp(300, '{ "message":"test"}'))
-    def test_wrapper(self, mocks):
+    @mock.patch.object(requests, 'get', return_value=Resp(300, '{ "message":"test"}'))
+    def test_request(self, mocks):
         with pytest.raises(Exception):
-            self.node.wrapper('api')
-        with mock.patch.object(requests, 'post', return_value=resp(300, '{ "message":"test"}')):
+            self.node.request('api')
+        with mock.patch.object(requests, 'post', return_value=Resp(300, '{ "message":"test"}')):
             with pytest.raises(Exception):
-                self.node.wrapper(api='/transactions/broadcast', post_data={"type": 4, "version": 2})
+                self.node.request(endpoint='/transactions/broadcast', post_data={"type": 4, "version": 2})
 
-    @mock.patch.object(requests, 'get', return_value=resp(200, 'test'))
-    def test_wrapper(self, mock):
+    @mock.patch.object(requests, 'get', return_value=Resp(200, 'test'))
+    def test_request(self, mock):
         api = '/blocks/last'
-        assert {'code': 200, 'text': 'test'} == self.node.wrapper(api)
-
+        assert {'code': 200, 'text': 'test'} == self.node.request(api)
 
     def test_broadcast(self):
         transaction = Transfer('3N6MFpSbbzTozDcfkTUT5zZ2sNbJKFyRtRj', 10000000)
-        with mock.patch.object(PublicNode, "wrapper", return_value={
+        with mock.patch.object(PublicNode, "request", return_value={
             'type': 4,
             'version': 3,
             'id': '74MeWagvnJ2MZTV7wEUQVWG8mTVddS9pJuqvtyG8b5eP',
@@ -73,44 +72,45 @@ class TestPublicNode:
             'proofs': ['j2q6isq2atpXBADMZ2Vz7oRozfUKGuDkLnVMqtnXkwDhw6tyHmMMHTbaVknP4JmYiVWN5PuNp6i4f5TBhuc9QSm']
         }
 
-    @mock.patch.object(PublicNode, 'wrapper')
+    @mock.patch.object(PublicNode, 'request')
     def test_compile(self, mock):
         self.node.compile('sddsfdsf')
         mock.assert_called()
 
-    @mock.patch.object(PublicNode, 'wrapper')
+    @mock.patch.object(PublicNode, 'request')
     def test_height(self, mock):
         self.node.height()
         mock.assert_called()
 
-    @mock.patch.object(PublicNode, 'wrapper')
+    @mock.patch.object(PublicNode, 'request')
     def test_last_block(self, mock):
         self.node.last_block()
         mock.assert_called()
 
-    @mock.patch.object(PublicNode, 'wrapper')
+    @mock.patch.object(PublicNode, 'request')
     def test_block(self, mock):
         self.node.block(20)
         mock.assert_called()
 
     def test_balance(self):
-        with mock.patch.object(PublicNode, "wrapper", return_value=1):
-            self.node.balance('3N6MFpSbbzTozDcfkTUT5zZ2sNbJKFyRtRj')
+        with mock.patch.object(PublicNode, "request", return_value={"balance": 5000_00000000}):
+            balance = self.node.balance('3N6MFpSbbzTozDcfkTUT5zZ2sNbJKFyRtRj')
+            assert balance == 5000_00000000
 
     def test_leasae_list(self):
-        with mock.patch.object(PublicNode, "wrapper"):
+        with mock.patch.object(PublicNode, "request"):
             self.node.lease_list('3N6MFpSbbzTozDcfkTUT5zZ2sNbJKFyRtRj')
 
     def test_sponsorship_list(self):
-        with mock.patch.object(PublicNode, "wrapper"):
+        with mock.patch.object(PublicNode, "request"):
             self.node.sponsorship_list('3N6MFpSbbzTozDcfkTUT5zZ2sNbJKFyRtRj')
 
     def test_transactions(self):
-        with mock.patch.object(PublicNode, "wrapper", return_value=1):
+        with mock.patch.object(PublicNode, "request", return_value=1):
             self.node.transactions('3N6MFpSbbzTozDcfkTUT5zZ2sNbJKFyRtRj')
 
     def test_tx(self):
-        with mock.patch.object(PublicNode, 'wrapper', return_value={
+        with mock.patch.object(PublicNode, 'request', return_value={
             'type': 4,
             'version': 3,
             'id': '74MeWagvnJ2MZTV7wEUQVWG8mTVddS9pJuqvtyG8b5eP',
